2ef5801628ad7425d104e17c36fed49ec31eb5e9
Umair Khan <125242896+mrumairkhan74@users.noreply.github.com>
2025-01-06 15:28:33 +0500

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-document.getElementById('button').addEventListener("click",function(){
-    document.querySelector('.popup').style.display='flex';
-})
-document.querySelector('#close').addEventListener("click",function(){
-    document.querySelector('.popup').style.display="none";
-})
-document.querySelector('.close').addEventListener("click",function(){
-    document.querySelector('.popup').style.display="none";
-})
